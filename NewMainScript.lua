f3ad3fb5f245dd7d084d003fe4a445ae6d9e649a
0123liskigi <77651380+0123liskigi@users.noreply.github.com>
2023-04-29 18:20:00 +0800

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -18,8 +18,8 @@ MainSection:NewButton("Inf Money For Everyone (Including You)", "Gives You Alot 
 
 local plyrs = game:GetService("Players")
 for _,P in pairs(plyrs:GetPlayers()) do
-   local N = {[1] = false, [2] = "inf", [3] = "Cash", [4] = P}
-    local UNN = {[1] = false, [2] = "9223372036854776000", [3] = "Cash", [4] = P}
+   local N = {[1] = false, [2] = "10000000000000000", [3] = "Cash", [4] = P}
+    local UNN = {[1] = false, [2] = "10000000000000000", [3] = "Cash", [4] = P}
     local SixNine = {[1] = false, [2] = SetMoney, [3] = "Cash", [4] = P}
     
  game:GetService("ReplicatedStorage").MoneyRequest:FireServer(unpack(N))
