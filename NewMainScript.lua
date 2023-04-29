0b131002bf297c399fcfa00ace86b81598c88ec2
0123liskigi <77651380+0123liskigi@users.noreply.github.com>
2023-04-29 18:26:47 +0800

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,11 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Hotel Elepahant Script By 0123liskigi", "Ocean")
 
---- ToggleUI
-Section:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.LeftCTRL, function()
-	Library:ToggleUI()
-end)
-
 -- Main
 local Main = Window:NewTab("Main")
 local MainSection = Main:NewSection("Main")
